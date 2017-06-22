f6b2603b84cb1fabd44d4083ced7e92edd69e283
amassignani <feelsgreat2016@gmail.com>
2017-06-22 23:54:17 +1200

    add problem solving

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// trying new cades and see if it works:
+
+// it works!
+// var click = 1;
+// if (click) {
+//   alert('welcome');
+// }
+// it works
+// var name = prompt("what s is yr name?"); alert("Welcome " + name + "to my Webpage" + "!");
+// document.addEventListener('DOMContentLoaded', start)
+//
+// this didn't work. yet.
+// function start () {
+//   page()
+// }
+// function page () {
+//   var page = document.getElementById("page")
+//   four.addEventListener('mouseenter', makeGreen)
+//   four.addEventListener('mouseleave', makeWhite)
+//   four.addEventListener('click', makeYellow)
+// }
+// function makeWhite (evt) {
+//   evt.target.style.backgroundColor = 'white'
+// }
+//
+// function makeGreen (evt) {
+//   evt.target.style.backgroundColor = 'green'
+// }
+//
+// function makeYellow (evt) {
+//   evt.target.style.backgroundColor = 'yellow'
+// }
